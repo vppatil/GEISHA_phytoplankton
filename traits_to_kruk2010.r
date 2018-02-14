e1c1ce1211e90fb7c88ada948bd6ee26780a5f05
Patil <vpatil@usgs.gov>
2018-02-13 16:10:03 -0900

    added new script with function to produce kruk 2010 morphological classification based on trait data

---
+++ traits_to_kruk2010.r
@@ -0,0 +1,18 @@
+traits.to.kruk=function(flagella=NA,silica=NA,MLD=NA,mucilage=NA,aerotopes=NA,SV=NA,V=NA)
+{
+	kruk.mfg=''
+	if(flagella==1 & silica==1) kruk.mfg='II'
+	if(flagella==1 & silica==0 & MLD < 2) kruk.mfg='I'
+	if(flagella==1 & silica==0 & MLD >= 2) kruk.mfg='V'
+	if(flagella==0 & silica==1) kruk.mfg='VI'
+	if(flagella==0 & silica==0 & mucilage==1 & aerotopes==1 & SV >=0.6) kruk.mfg='III'
+	if(flagella==0 & silica==0 & mucilage==1 & aerotopes==1 & SV < 0.6) kruk.mfg='VII'
+	if(flagella==0 & silica==0 & mucilage==1 & aerotopes==0 & SV < 10) kruk.mfg='I'
+	if(flagella==0 & silica==0 & mucilage==1 & aerotopes==0 & SV >= 10) kruk.mfg='VII'
+	if(flagella==0 & silica==0 & mucilage==0 & V < 30 & MLD <20) kruk.mfg='I'
+	if(flagella==0 & silica==0 & mucilage==0 & V < 30 & MLD >= 20) kruk.mfg='IV'
+	if(flagella==0 & silica==0 & mucilage==0 & V >= 30 & aerotopes==1 ) kruk.mfg='III'
+	if(flagella==0 & silica==0 & mucilage==0 & V >= 30 & aerotopes==0 ) kruk.mfg='IV'
+
+	return(kruk.mfg)
+}
