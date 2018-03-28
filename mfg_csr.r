18a9265ef0fb4d297cdc4b7a5063739515e8a596
Patil <vpatil@usgs.gov>
2018-03-28 16:36:42 +0200

    modified mfg_csr functions so they do not require splitting an mfg name into an MFG.number column. also added some documentation to the mfg_csr functions.

--- mfg_csr.r
+++ mfg_csr.r
@@ -1,47 +1,50 @@
-##not clear how function this is as of sept 19, 2017
-##presumably need to load mfg csr library
-
+##need to load mfg csr library
+#must be in the working directory for the MFG_CSR.rda function
 load('MFG_CSR.rda')
+
 #contains the mfg_csr correspondence library developed by orlane, nico, et al at workshop 2.
 
-mfg.csr.convert<-function(mfg.num)
+#returns a CSR classification for a single MFG
+mfg.csr.convert<-function(mfg)
 {
   #requires loading MFG_CSR.rda
   #want to pull out the mfg number in an intelligent way.
   #if this is run with something returned from your library, no problem.
   
-  if(mfg.num %in% mfg.csr$MFG.number==F){csr=NA}
+  if(mfg %in% mfg.csr$MFG.name==F){csr=NA}
   else
   {
-	csr=mfg.csr$CSR[mfg.csr$MFG.number==mfg.num]
+	csr=mfg.csr$CSR[mfg.csr$MFG.name==mfg]
 	return(csr)
   }
 
 }
 
-mfg.csr.convert.df=function(phyto.df,mfg.num)
+#return a new dataframe with a CSR column, assuming the dataframe has an MFG column to start.
+mfg.csr.convert.df=function(phyto.df,mfg)
 {
   csrs<-vector(length=dim(phyto.df)[1])
   for(i in 1:dim(phyto.df)[1])
   {
-    csrs[i]=mfg.csr.convert(phyto.df[[mfg.num]][i])
+    csrs[i]=mfg.csr.convert(phyto.df[[mfg]][i])
   }
   phyto.df$CSR=csrs
   return(phyto.df)
 }
 
+
+##chain functions to generate a CSR classification with only a genus and species names as inputs
 spp.csr.convert=function(genus,species)
 {
 	mfg=nico.phyto.convert(genus,species)
-	mfg.num=strsplit(mfg,split='-')[[1]][1]
-	csr=mfg.csr.convert(mfg.num)
+	csr=mfg.csr.convert(mfg)
 	return(csr)
 }
 
+##CSR column for whole data.frame, based on the presence of columns named genus and species
 spp.csr.convert.df=function(phyto.df)
 {
 	phyto.df=phyto.convert.df(phyto.df)
-	phyto.df$MFG.num=sapply(orlane$MFG.from.Nico,function(x) strsplit(x,split='-')[[1]][1])
-	phyto.df=mfg.csr.convert.df(phyto.df,'MFG.num')
+	phyto.df=mfg.csr.convert.df(phyto.df,'MFG')
 	return(phyto.df)
 }
