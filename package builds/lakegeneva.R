e2b5100bd1f618a6edc8c7bb25871e8e7615dbd2
Patil <vpatil@usgs.gov>
2018-10-12 17:57:37 -0800

    algaeclassify build 0.1.1 and associated scripts. Now includes libraries for species to mfg and mfg to csr conversion. also includes script to create MFG classification without size. need to clarify some things in the documentation. See algaeClassify.pdf for current documentation.

---
+++ package builds/lakegeneva.R
@@ -0,0 +1,16 @@
+#' example dataset from lake Geneva, Switzerland (50 lines)
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{lake}{lake name}
+#'  \item{date_dd_mm_yy}{date dd-mm-yy format}
+#'  \item{phyto_name}{phytoplankton species binomial name}
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(lakegeneva)
+#'
+#' @keywords datasets
+#'
+"lakegeneva"
