142626bd4aa753f0463bfc65f68e24f2a7c275d7
Vijay Patil <vpatil@usgs.gov>
2020-04-24 13:06:41 -0800

    adding csv data files for expanded and original species to mfg libraries.

---
+++ package builds/spp to mfg and mfg to csr functions/species.mfg.library.r
@@ -0,0 +1,20 @@
+#' Trait-based MFG classifications for common Eurasion/North American phytoplankton species.
+#' See accompanying manuscript for sources
+#'
+#' @format A data frame with columns:
+#' \describe{
+#'  \item{genus}{genus name}
+#'  \item{species}{species name}
+#'  \item{MFG}{corresponding MFG classification based on Salmaso et al. 2015}
+#'  \item{MFG2}{corresponding MFG classification based on Salmaso et al. 2015}
+#'  \item{Habitat}{corresponding MFG classification based on Salmaso et al. 2015}
+#'  \item{Note}{corresponding MFG classification based on Salmaso et al. 2015}
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(species.mfg.library)
+#'
+#' @keywords datasets
+#'
+"species.mfg.library"
